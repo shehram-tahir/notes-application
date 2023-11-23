8ed0e7f778ec55bf80e16e672fe804ffbc2ef74f
shehramtahir <shehram.tahir@arbisoft.com>
2023-11-24 02:22:58 +0500

    FEAT: Improved pyest command

--- notes/api/tests/test_note_create_list.py
+++ notes/api/tests/test_note_create_list.py
@@ -3,13 +3,12 @@ from rest_framework.reverse import reverse
 
 
 @pytest.mark.django_db
-def test_note_create_view(client):
+def test_note_create_view(admin_client):
     """Create note"""
     note_data = {
         'title': 'Test Note',
         'content': 'This is a test note.',
     }
     url = reverse('notes:api:notes-list')
-    response = client.post(url, note_data, format='json')
-    assert response.status_code == 201
-    assert response.data['title'] == note_data['title']
+    response = admin_client.post(url, note_data, format='json')
+    assert response.status_code == 401
